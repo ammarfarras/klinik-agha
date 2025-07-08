f11db976c068e67bdf8241794ef82b09aa6586f9
Ammar Farras <ammarfarras@MacBook-Air-Ammar.local>
2025-07-09 01:14:50 +0700

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,12 @@
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { NavLink, Navigate, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import "./../styles/Navbar.css";
 import { Container, Button, Dropdown } from "react-bootstrap";
-import DropdownMenu from "react-bootstrap/esm/DropdownMenu";
-import logoAgha from "../assets/agha fix.png"
+import logoAgha from "../assets/agha fix.png";
 
 function BasicExample() {
-  const isLoggedIn = JSON.parse(localStorage.getItem("idUser")); // // true or false
-
+  const isLoggedIn = JSON.parse(localStorage.getItem("idUser")); // object berisi name, email, img, dll
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -16,56 +14,44 @@ function BasicExample() {
     window.location.reload();
   };
 
-  let component = "";
+  let accountDropdown = "";
   if (isLoggedIn) {
-    component = (
-      <>
-        <div className="text-center">
-          <Dropdown>
-            <Dropdown.Toggle id="dropdown-basic">Account&nbsp;</Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <div className="text-center mx-4">
-                <img
-                  height={50}
-                  className="text-center justify-center"
-                  src={isLoggedIn.img}
-                />
-
-                <h5 className="ms-auto text-center">{isLoggedIn.name}</h5>
-                <p>{isLoggedIn.email}</p>
-
-                <Button
-                  onClick={handleLogout}
-                  // onClick={window.localStorage.clear()}
-                  className="logindong text-white text-carevul border-carevul py-2"
-                >
-                  Logout
-                </Button>
-              </div>
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-      </>
+    accountDropdown = (
+      <div className="text-center">
+        <Dropdown>
+          <Dropdown.Toggle id="dropdown-basic">Account&nbsp;</Dropdown.Toggle>
+          <Dropdown.Menu>
+            <div className="text-center mx-4">
+              <img height={50} src={isLoggedIn.img} alt="user" />
+              <h5 className="text-center">{isLoggedIn.name}</h5>
+              <p>{isLoggedIn.email}</p>
+              <Button
+                onClick={handleLogout}
+                className="logindong text-white text-carevul border-carevul py-2"
+              >
+                Logout
+              </Button>
+            </div>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
     );
   } else {
-    component = (
-      <>
-        <div className="text-center ">
-          <NavLink
-            to={"/login"}
-            className="logindong btn text-carevul border-carevul m-1"
-          >
-            Login
-          </NavLink>
-          <NavLink
-            to={"/regis"}
-            className="btn color-carevul-gradient text-white m-1"
-          >
-            Register
-          </NavLink>
-        </div>
-      </>
+    accountDropdown = (
+      <div className="text-center">
+        <NavLink
+          to={"/login"}
+          className="logindong btn text-carevul border-carevul m-1"
+        >
+          Login
+        </NavLink>
+        <NavLink
+          to={"/regis"}
+          className="btn color-carevul-gradient text-white m-1"
+        >
+          Register
+        </NavLink>
+      </div>
     );
   }
 
@@ -73,10 +59,9 @@ function BasicExample() {
     <Navbar bg="light" expand="lg">
       <Container className="mx-auto">
         <Navbar.Brand href="#">
-          {" "}
           <div className="logoApp" onClick={() => navigate("/")}>
-            <img width={120} src={logoAgha} alt="Carevul Logo" />{" "}
-          </div>{" "}
+            <img width={120} src={logoAgha} alt="Carevul Logo" />
+          </div>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -87,8 +72,14 @@ function BasicExample() {
             <NavLink to={"/consult/category"} className="nav-link">
               Konsultasi
             </NavLink>
+
+            {isLoggedIn && isLoggedIn.email === "admin@gmail.com" && (
+              <NavLink to={"/admin"} className="nav-link text-danger fw-bold">
+                Admin
+              </NavLink>
+            )}
           </Nav>
-          <Nav className="ms-auto gap-1">{component}</Nav>
+          <Nav className="ms-auto gap-1">{accountDropdown}</Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
